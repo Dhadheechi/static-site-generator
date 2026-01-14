ab8743dd3cb23c5fb8a8ce27adfd58f02025093f
dhadheechi <rdhadheechi@gmail.com>
2026-01-14 21:34:46 +0530

    rebuild

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-python3 src/main.py "/static-site-generator/"
+python3 src/main.py "/static-site-generator"
 # build.sh just runs the static site generator which generates all the pages recursively. It's not meant to open up a local host at all. 
