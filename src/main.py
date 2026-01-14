1555fb241b78e24ecaa7223a3c7dff57cdc8b0bb
dhadheechi <rdhadheechi@gmail.com>
2026-01-14 21:06:55 +0530

    made the basepath configurable

--- src/main.py
+++ src/main.py
@@ -2,14 +2,15 @@ from textnode import TextNode, TextType
 from generate import generate_page, generate_pages_recursively
 
 import os
+import sys
 import shutil
 def copy_static_to_public(dir_path, abs_path="."):
     if dir_path == ".":
-        if os.path.exists(os.path.join(abs_path, "public")):
-            shutil.rmtree(os.path.join(abs_path, "public")) # delete everything from the destination on the first invocation of this recursive function
-        os.mkdir(os.path.join(abs_path, "public"))
+        if os.path.exists(os.path.join(abs_path, "docs")):
+            shutil.rmtree(os.path.join(abs_path, "docs")) # delete everything from the destination on the first invocation of this recursive function
+        os.mkdir(os.path.join(abs_path, "docs"))
     current_dir = os.path.join(abs_path, f"static/{dir_path}")
-    current_dir_public = os.path.join(abs_path, f"public/{dir_path}")
+    current_dir_public = os.path.join(abs_path, f"docs/{dir_path}")
     for f in os.listdir(current_dir):
         path = os.path.join(current_dir, f)
         if os.path.isfile(path):
@@ -26,5 +27,9 @@ def copy_static_to_public(dir_path, abs_path="."):
 
 
 if __name__ == "__main__":
+    if not len(sys.argv) > 1:
+        basepath = "/"
+    else:
+        basepath = sys.argv[1] 
     copy_static_to_public(".")
-    generate_pages_recursively("content/", "template.html", "public")
+    generate_pages_recursively("content", "template.html", "docs", basepath)
